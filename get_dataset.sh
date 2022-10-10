b0db3bfab2f8ffcf19b1e19c25a14e0a06ce87cd
weiyuan-c <87663784+weiyuan-c@users.noreply.github.com>
2022-10-10 15:04:37 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -4,6 +4,3 @@ gdown 1LMIaOY8NSKWmGtbvTsjXcHVaYKnNN_9u -O hw1_data.zip
 # # Unzip the downloaded zip file
 mkdir hw1_data
 unzip ./hw1_data.zip -d hw1_data
-
-wget -O model1.ckpt 'https://www.dropbox.com/s/a0o8feccpugtwix/model1.ckpt?dl=1'
-wget -O model2.ckpt 'https://www.dropbox.com/s/0ty3tsy2sa0jfgv/model2.ckpt?dl=1'
