4ad3b2b145e5a1363696c12592e207e48f6bb6b3
weiyuan-c <87663784+weiyuan-c@users.noreply.github.com>
2022-10-10 15:05:28 +0800

    Add files via upload

---
+++ hw1_download.sh
@@ -0,0 +1,8 @@
+# models for p1
+wget -O model1.ckpt 'https://www.dropbox.com/s/a0o8feccpugtwix/model1.ckpt?dl=1'
+wget -O model2.ckpt 'https://www.dropbox.com/s/0ty3tsy2sa0jfgv/model2.ckpt?dl=1'
+
+# models for p2
+wget -O p2_model1.ckpt 'https://www.dropbox.com/s/2fw9hljftw1hmnu/p2_mode1l.ckpt?dl=1'
+wget -O p2_model2.ckpt 'https://www.dropbox.com/s/8ds6vbi42u32wi3/p2_model2.ckpt?dl=1'
+wget -O p2_model3.ckpt 'https://www.dropbox.com/s/0i6z62qmt4b2ab2/p2_model3.ckpt?dl=1'
