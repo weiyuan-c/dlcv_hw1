bdf1378ac71519168503ca13a5b63e482aa1276f
weiyuan-c <87663784+weiyuan-c@users.noreply.github.com>
2022-10-30 01:32:15 +0800

    Update hw1_2.sh

--- hw1_2.sh
+++ hw1_2.sh
@@ -1,1 +1,1 @@
-python3 p2.py --test_dir $1 --output_dir $2 --weight1 p2_model1.ckpt --weight2 p2_model2.ckpt --weight3 p2_model3.ckpt
+python3 p2.py --test_dir $1 --output_dir $2 --weight1 p2_model1.ckpt --weight2 p2_model2.ckpt --weight3 p2_model3.ckpt --batch_size 16
